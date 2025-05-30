000c37449289714fb42960045bbb9ec25583df2b
Ohkubo KOHEI <o@kbn.one>
2025-05-30 14:41:44 +0900

    fix cache expiring

--- lib/cache.ts
+++ lib/cache.ts
@@ -1,7 +1,6 @@
 // Cache API を悪用して任意の json を cache する
 
 const CACHE_TTL = 10 * 60 * 1000; // 10 minutes in milliseconds
-
 export async function cache<T>(
   key: string,
   getter: () => Promise<T>,
@@ -10,12 +9,15 @@ export async function cache<T>(
   const dummyRequest = new Request("https://example.com/" + key);
   const cachedResponse = await cache.match(dummyRequest);
   if (cachedResponse) {
-    console.log("cache hit");
-    return cachedResponse.json();
+    const expireAt = cachedResponse.headers.get("X-EXPIRE-AT");
+    if (expireAt && Number(expireAt) > Date.now()) {
+      console.log("cache hit");
+      return cachedResponse.json();
+    }
   }
   const value = await getter();
   const dummyResponse = Response.json(value);
-  dummyResponse.headers.append("Cache-Control", `max-age=${CACHE_TTL}`);
+  dummyResponse.headers.append("X-EXPIRE-AT", (Date.now() + CACHE_TTL).toString());
   await cache.put(dummyRequest, dummyResponse);
   return value;
 }
