50e951bd1d43122319920a9e9dabed5fe71ba5ac
Ohkubo KOHEI <o@kbn.one>
2025-12-15 10:21:08 +0000

    add cronCount

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
 import { app } from "./lib/bolt.ts";
 
-// JST 09:01 に実行。 9時より前だと Date がずれるので念の為。
-Deno.cron("countUsers", "1 0 * * *", async () => {
+// JST 09:05 に実行。 9時より前だと Date がずれるので念の為。
+let cronCount = 0;
+Deno.cron("countUsers", "5 0 * * *", async () => {
+  cronCount++;
+  console.log("running cron: count=", cronCount)
   await import("./crons/countUsers.ts").then((x) => x.run());
 });
 
